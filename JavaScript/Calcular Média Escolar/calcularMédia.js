c125f96ec3ea6ae84ec0c6356b4b20872b52e6d0
Paitax <69782755+Pedro-Paitax@users.noreply.github.com>
2022-07-04 11:11:04 -0300

    Update calcularMédia.js

--- JavaScript/Calcular Média Escolar/calcularMédia.js
+++ JavaScript/Calcular Média Escolar/calcularMédia.js
@@ -1,26 +1,29 @@
-let nota = 90 
+function getMedia(nota){
 
-
-let scoreA = nota >= 90 && nota <= 100
-let scoreB = nota >= 80 && nota <= 89
-let scoreC = nota >= 70 && nota <= 79
-let scoreD = nota >= 60 && nota <= 69
-let scoreF = nota < 70
-
-let mediaFinal
-
-if( scoreA ){
-    mediaFinal = "A"
-}else if( scoreB ){
-    mediaFinal = "B"   
-}else if( scoreC ){
-    mediaFinal = "C"
-}else if( scoreD ){
-    mediaFinal = "D"
-}else if( scoreF ){
-    mediaFinal = "F"
-}else{
-    console.log('Nota Inválida')
+    let scoreA = nota >= 90 && nota <= 100
+    let scoreB = nota >= 80 && nota <= 89
+    let scoreC = nota >= 70 && nota <= 79
+    let scoreD = nota >= 60 && nota <= 69
+    let scoreF = nota >= 0 && nota < 60
+    
+    let mediaFinal
+    
+    if( scoreA ){
+        mediaFinal = "A"
+    }else if( scoreB ){
+        mediaFinal = "B"   
+    }else if( scoreC ){
+        mediaFinal = "C"
+    }else if( scoreD ){
+        mediaFinal = "D"
+    }else if( scoreF ){
+        mediaFinal = "F"
+    }else{
+        console.log('Nota Inválida')
+    }
+    
+    return mediaFinal
 }
 
-console.log(mediaFinal)
+
+
